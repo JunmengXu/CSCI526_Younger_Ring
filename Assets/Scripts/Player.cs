a2eaacd731868199682966a65c39db64c86b8688
charliequsc <gepingqu@usc.edu>
2022-09-23 11:52:19 -0700

    update tutorial

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -96,11 +96,11 @@ public class Player : MonoBehaviour
         isGrounded = true;
         playerColor.ChangeColorAndLayer();
     }
-    private void OnTriggerEnter2D(Collider2D other) 
-    {
-        if (other.gameObject.layer == 15) {
-            Debug.Log("lol");
-            Destroy(other.gameObject);
-        }
-    }
+    // private void OnTriggerEnter2D(Collider2D other) 
+    // {
+    //     if (other.gameObject.layer == 15) {
+    //         Debug.Log("lol");
+    //         Destroy(other.gameObject);
+    //     }
+    // }
 }
