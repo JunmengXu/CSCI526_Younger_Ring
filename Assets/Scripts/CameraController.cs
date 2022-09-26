892023fb7c7ca664d77b4e33f69360638cc0cd02
MikeShuyang <wangshuy@usc.edu>
2022-09-26 15:24:24 -0700

    Updated camera-follow logic
    Updated the "CameraController" script just so the camera will smoothly follow the player's current position. Updated the scenes to apply this change.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,25 +1,35 @@
+using System;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Used to get the player's current position
-    public Player player;
+    // Player transform
+    public Transform playerTransform;
+    // Main camera transform
+    public Transform mainCameraTransform;
+    
+    // The following lag between the player and the camera
+    public float cameraFollowTimeOffset = 3;
 
-    /// <summary>
-    /// Parameters of the main camera
-    /// </summary>
-    public float initialPosition;
-    public float cameraHeight;
-    public Camera mainCamera;
-
-    private void FixedUpdate()
+    private void Update()
     {
         // Get the main camera's current position
-        Transform cameraTransform = mainCamera.transform;
-        Vector3 currentCameraPosition = cameraTransform.position;
+        Vector3 cameraStartPosition = mainCameraTransform.position;
+        // Get the player's current position
+        Vector3 playerPosition = playerTransform.position;
+
+        // Create the target Vector3 position variable for the main camera to lerp
+        Vector3 targetCameraPosition = new Vector3(
+            playerPosition.x,
+            playerPosition.y,
+            cameraStartPosition.z
+        );
         
-        // Move the main camera's position by the unit of cameraHeight according to the player's position
-        currentCameraPosition.y = initialPosition + (int)(player.transform.position.y / cameraHeight) * cameraHeight;
-        cameraTransform.position = currentCameraPosition;
+        // Lerp to gradually drag the camera's position towards the player
+        mainCameraTransform.position = Vector3.Lerp(
+            cameraStartPosition, 
+            targetCameraPosition, 
+            cameraFollowTimeOffset * Time.deltaTime
+        );
     }
 }
