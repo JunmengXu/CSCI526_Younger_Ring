1f5b596723d96124270f27fbb702252d0aae7cb8
Wenzheng Guo <gwz1805504@gmail.com>
2022-12-01 02:24:47 -0800

    add background to levels, and background checkbox

---
+++ Assets/Background.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void onToogle(bool show)
+    {
+        this.enabled = show;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
